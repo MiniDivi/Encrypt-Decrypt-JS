b8769385c2c4c9c281071eabab2329d5442b3e35
MiniDivi <alessandro.divincenzo77@gmail.com>
2022-03-25 14:21:56 +0100

    Dectypt implementato

--- js/script.js
+++ js/script.js
@@ -21,11 +21,10 @@ $(document).ready(function () {
         for (let i = 0; i < wordToEncryptLenght; i++) {
             for (let j = 0; j < alphabet.length; j++) {
                 if (alphabet[j] === wordToEncrypt.charAt(i)) {
-                    if(keyValuesCounter == keyValuesArray.length){
+                    if (keyValuesCounter == keyValuesArray.length) {
                         keyValuesCounter = 0;
                     }
                     encryptedWord += getLetter((+j + +keyValuesArray[keyValuesCounter]) % alphabet.length);
-
                     keyValuesCounter++;
 
                     break;
@@ -34,28 +33,81 @@ $(document).ready(function () {
         }
         $('#encrypted-word').html("<b>Encrypted word: </b>" + encryptedWord);
     });
+
+    $("#decrypt-form").submit(function (e) {
+        e.preventDefault();
+
+        let wordToDecrypt = $('#word-to-decrypt').val();
+        //*il metodo wordToDecrypt.length non funzionava quindi ho dovuto procedere con la seguente soluzione
+        let wordToDecryptLenght = [...wordToDecrypt].reduce(a => a + 1, 0);
+        let decryptedWord = "";
+
+        const keyValuesArrayDecrypt = Array();
+
+
+        for (let i = 0; i < numberOfKeys; i++) {
+            keyValuesArrayDecrypt.push($("#D" + i).val());
+        }
+
+        var keyValuesCounter = 0;
+
+        for (let i = 0; i < wordToDecryptLenght; i++) {
+            for (let j = 0; j < alphabet.length; j++) {
+                if (alphabet[j] === wordToDecrypt.charAt(i)) {
+                    if (keyValuesCounter == keyValuesArrayDecrypt.length) {
+                        keyValuesCounter = 0;
+                    }
+                    
+                    decryptedWord += (getLetter((alphabet.length + (+j - +keyValuesArrayDecrypt[keyValuesCounter])) % alphabet.length));
+                    keyValuesCounter++;
+
+                    break;
+                }
+            }
+        }
+        $('#decrypted-word').html("<b>Decrypted word: </b>" + decryptedWord);
+    });
+
+
 });
 function generateAlphabeth() {
     return [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
 }
 
-function getLetter(number){
+function getLetter(number) {
+    console.log("alphabet: " + alphabet[number]);
     return alphabet[number];
 }
 
-function getNumberOfKeys() {
-    numberOfKeys = $('#number-of-keys').val();
+function getNumberOfKeysEncrypt() {
+    numberOfKeys = $('#number-of-keys-encrypt').val();
     if (numberOfKeys > 6) {
         numberOfKeys = 6;
     }
-    showKeyValueInput();
+    showKeyValueInput(true);
+}
+function getNumberOfKeysDecrypt() {
+    numberOfKeys = $('#number-of-keys-decrypt').val();
+    if (numberOfKeys > 6) {
+        numberOfKeys = 6;
+    }
+    showKeyValueInput(false);
 }
 
-function showKeyValueInput() {
+function showKeyValueInput(EncryptOrDecypt) { //*Encrypt = true; Decrypt = false;
     let code = "";
-    for (let i = 0; i < numberOfKeys; i++) {
-        code += '<label for="C' + i + '" class="me-3">C' + i + ': </label>';
-        code += '<input type="number" class="mb-3" name="C' + i + '" id="C' + i + '" min="1" max="25" required> <br>';
+    if (EncryptOrDecypt) {
+        for (let i = 0; i < numberOfKeys; i++) {
+            code += '<label for="C' + i + '" class="me-3 ms-2">C' + i + ': </label>';
+            code += '<input type="number" class="mb-3" name="C' + i + '" id="C' + i + '" min="1" max="25" required> ';
+        }
+        $('keyValuesEncrypt').html(code); 
+    } else {
+        for (let i = 0; i < numberOfKeys; i++) {
+            code += '<label for="D' + i + '" class="me-3 ms-2">C' + i + ': </label>';
+            code += '<input type="number" class="mb-3" name="D' + i + '" id="D' + i + '" min="1" max="25" required> ';
+        }
+        $('keyValuesDecrypt').html(code);
+
     }
-    $('keyValues').html(code);
 }
